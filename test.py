aeb1bccd15af5cec5535313bd34b9701bd0776df
Jaray-zhou <61246636+Jaray-zhou@users.noreply.github.com>
2020-05-13 22:40:02 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import random
 
+
+
 n = random.randint(1,5)
+print(n)
 sum = 0
-for i in n:
-  print("this is number of :" + i)
-  sum += i*2
-print("the sum is: " + sum)
+
+for i in range(n):
+    print("this is number of :" + str(i))
+    sum += i*2
+
+print("the sum is: " + str(sum))
