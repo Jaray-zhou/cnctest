2a733b864178be6a750dba5d9ccff849e4a7818e
Jaray-zhou <61246636+Jaray-zhou@users.noreply.github.com>
2020-05-14 10:24:41 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 from flask import Flask
 import random
 
-cnctest = Flaks(__name__)
+cnctest = Flask(__name__)
 
 @cnctest.route('/')
-del output():
+def output():
     n = random.randint(1,5)
     sum = 0
 
     for i in range(n):
         print("this is number of :" + str(i))
-        sum += i*2
-    return "the sum is: " + str(sum))
+        sum += i
+    return "the sum is: " + str(sum)
         
 if __name__ =='__main__':
     cnctest.run(debug=True, host='0.0.0.0')
